75b2bf088617bc8737cba4fd97091bd5da9c5604
Reginald J Dixon <vassarjonez@yahoo.com>
2025-02-06 16:12:56 -0500

    made update in notes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Survey from "./components/Survey";
 function App() {
   return (
     <div className="App">
-      <h2>React SurveyJS Example</h2>
+      <h2>Survey Example</h2>
       <Survey />
     </div>
   );
